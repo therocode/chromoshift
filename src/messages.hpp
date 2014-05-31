7fae2ff2b465f284de7cd2f362b6901d11e921a5
Tobias Widlund <widlundtobias@gmail.com>
2014-06-01 02:41:29 +0800

    added position and color messages for the player and the renderer reacts

--- src/messages.hpp
+++ src/messages.hpp
@@ -5,9 +5,12 @@
 namespace sf
 {
     class Image;
+    class Color;
 }
 
 FEA_DECLARE_MESSAGE(QuitMessage);
 FEA_DECLARE_MESSAGE(MoveMessage, Direction);
 FEA_DECLARE_MESSAGE(BGMessage, const sf::Image&);
 FEA_DECLARE_MESSAGE(ResizeMessage, uint32_t, uint32_t);
+FEA_DECLARE_MESSAGE(PlayerPositionMessage, uint32_t, uint32_t);
+FEA_DECLARE_MESSAGE(PlayerColorMessage, const sf::Color&);
